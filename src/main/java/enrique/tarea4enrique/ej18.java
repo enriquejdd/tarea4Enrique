b708e49ccc9813ad37af0b7d488f3502ac5b37b6
adrivr99 <adri.vallejo.99@gmail.com>
2020-12-07 21:41:33 +0100

    Creacion main

---
+++ src/main/java/enrique/tarea4enrique/ej18.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enrique.tarea4enrique;
+
+/**
+ *
+ * @author adriv
+ */
+public class ej18 {
+    public static void main(String[] args) {
+        
+    }
+}
