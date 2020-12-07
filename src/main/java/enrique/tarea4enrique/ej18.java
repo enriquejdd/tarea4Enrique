a97085b8222108e193fa931e01e725e9bf0f60a0
adrivr99 <adri.vallejo.99@gmail.com>
2020-12-07 21:52:36 +0100

    Añade objeto en ej18

--- src/main/java/enrique/tarea4enrique/ej18.java
+++ src/main/java/enrique/tarea4enrique/ej18.java
@@ -11,6 +11,15 @@ package enrique.tarea4enrique;
  */
 public class ej18 {
     public static void main(String[] args) {
+        ClaseInventada fecha = new ClaseInventada(15, 8, 1999);
         
+        ClaseInventada mundial = new ClaseInventada (27, 7, 2010);
+        
+        ClaseInventada fechaHoy = new ClaseInventada (7, 12, 2020);
+        
+        System.out.println("Fecha: " + fecha);
+        System.out.println("Mundial : " + mundial);
+        System.out.println("Fecha Hoy : " + fechaHoy);
     }
+    
 }
